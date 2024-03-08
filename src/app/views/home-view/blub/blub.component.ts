552a8ca884e9e5e9d4932d12370339d9e3e0d73a
Yacineos <psmartyacine@gmail.com>
2024-03-08 18:36:30 +0100

    blob overflow limited

--- src/app/views/home-view/blub/blub.component.ts
+++ src/app/views/home-view/blub/blub.component.ts
@@ -1,4 +1,14 @@
-import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  Input,
+  ViewChild,
+  signal,
+  AfterViewInit,
+  effect,
+  OnDestroy,
+  EffectRef,
+} from '@angular/core';
 import { Coor } from '../../../core/models/types';
 
 @Component({
@@ -6,22 +16,42 @@ import { Coor } from '../../../core/models/types';
   standalone: true,
   imports: [],
   templateUrl: './blub.component.html',
-  styleUrl: './blub.component.css'
+  styleUrl: './blub.component.css',
 })
-export class BlubComponent {
-  @ViewChild("blub") bulb !: ElementRef ;
-  
-  @Input({required: true}) color!: string;
-  @Input() mouseCoor !: Coor ;
+export class BlubComponent implements AfterViewInit, OnDestroy {
+  @ViewChild('blub') bulb!: ElementRef;
 
-  ngOnChanges() {
-    if (this.bulb && this.mouseCoor) {
-      this.bulb.nativeElement.animate({
-        left:`${this.mouseCoor.x}px` ,
-        top: `${this.mouseCoor.y}px`
-      },{ duration: 3000, fill: "forwards"}) ;
-      //this.bulb.nativeElement.style.top = `${this.mouseCoor.y}px`;
-    }
+  @Input({ required: true }) color!: string;
+
+  private _sigMouse = signal<Coor>({ x: 0, y: 0 });
+  @Input()
+  get mouseCoor() {
+    return this._sigMouse();
+  }
+  set mouseCoor(c: Coor) {
+    this._sigMouse.set(c);
   }
 
+  private effectRef?: EffectRef;
+  ngOnDestroy(): void {
+      this.effectRef?.destroy()
+  }
+  
+  ngAfterViewInit() {
+  }
+
+  constructor() {
+    this.effectRef = effect(() => {
+      if (this.bulb && this.mouseCoor) {
+        this.bulb.nativeElement.animate(
+          {
+            left: `${this.mouseCoor.x}px`,
+            top: `${this.mouseCoor.y}px`,
+          },
+          { duration: 3000 , fill:"forwards" }
+        );
+        //this.bulb.nativeElement.style.top = `${this.mouseCoor.y}px`;
+      }
+    });
+  }
 }
--- status.md
+++ status.md
@@ -1,11 +1,12 @@
 # TODO
-     
+    * find a solution for the blob in mobile version
 # BLOCKED
     * failed to add jest to implement tests
 # DONE
     * set up the project
     * implement bulb following cursor
     * be as modular as possible in the bulb component  
+    * limite the blob to the screen and avoid having overflow
+
 # DOING
-    * finish the bulb component 
-    * home component to capture mouse coordinates and do the computing and send the coordinates to the bulb element 
+    
