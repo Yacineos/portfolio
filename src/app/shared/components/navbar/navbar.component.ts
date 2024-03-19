2ac5a29a4d4d8852c222f3cd3e70558617a981ba
Yacineos <psmartyacine@gmail.com>
2024-03-19 12:14:19 +0100

    added the svg's for the toggle white dark mode , and added ng class on the navbar to change with the mode

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, computed } from '@angular/core';
+import { AppearanceService } from '../../services/appearance.service';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+
+  sigIsDark = computed(() => this.appearanceService.sigDarkMode());
+
+  constructor(private appearanceService: AppearanceService){
+
+  }
 
 }
--- src/app/shared/components/theme-toggle-button/theme-toggle-button.component.ts
+++ src/app/shared/components/theme-toggle-button/theme-toggle-button.component.ts
@@ -1,14 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input, computed, signal } from '@angular/core';
 import { AppearanceService } from '../../services/appearance.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-theme-toggle-button',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './theme-toggle-button.component.html',
   styleUrl: './theme-toggle-button.component.css'
 })
 export class ThemeToggleButtonComponent {
+  
+  sigIsDark = computed(() => this.appearanceService.sigDarkMode());
 
   constructor(private appearanceService:AppearanceService){
 
