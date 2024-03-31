10e39a40adbc6cfbc97887fdaee95e8e77e1ae0e
Yacineos <psmartyacine@gmail.com>
2024-03-31 08:48:59 +0200

    test toggle theme button new position

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -2,11 +2,12 @@ import { ChangeDetectionStrategy, Component, computed } from '@angular/core';
 import { AppearanceService } from '../../services/appearance.service';
 import { NgClass } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { ThemeToggleButtonComponent } from '../theme-toggle-button/theme-toggle-button.component';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [NgClass,RouterLink],
+  imports: [NgClass,RouterLink,ThemeToggleButtonComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush
